ae231cb0ebb1d9eb7876bc502efe0a7910fd6fc7
Vivek <pvivek@live.in>
2025-06-19 18:26:37 +0000

    wired the new logging into barebones langchain module

--- main.py
+++ main.py
@@ -1,23 +1,15 @@
 import os
 from dotenv import load_dotenv
 from langchain_community.chat_models import ChatOpenAI
-import structlog
+import logging_setup
 
 OPENAI_KEY_NAME="OPENAI_API_KEY"
 load_dotenv()
-structlog.configure(
-    processors=[
-        structlog.processors.JSONRenderer(),
-    ],
-    context_class=dict,
-    logger_factory=structlog.PrintLoggerFactory,
-    wrapper_class=structlog.stdlib.BoundLogger,
-)
-logger = structlog.get_logger()
 
 
 def main():
-    print(f"OpenAI Key = {os.getenv(OPENAI_KEY_NAME)}")
+    logger = logging_setup.Logger()
+    logger.console_log.info(f"OpenAI Key = {os.getenv(OPENAI_KEY_NAME)}")
 
 if (__name__=="__main__"):
     main()
